24d011f709878785021bcfe81b4c4e8f4bbe2e9a
jared.mauck <jaredmauck@gmail.com>
2022-12-05 00:14:26 -0800

    updates - NOT DONE

--- Day 4/main.py
+++ Day 4/main.py
@@ -1,4 +1,34 @@
+import re
+
 with open("input.txt", "r") as input:
-    input_text = input.read()
+    input_text_file = input.read()
+
+input_format = input_text_file.replace(",", "\n")
+input_text = input_format.replace("-", "\n")
+
 input_list = input_text.split("\n")
+# print(input_text)
+# formatted_list = []
+# for i in input_list:
+#     if i == '':
+#         formatted_list.append([])
+#     else:
+#         formatted_list[-1].append(int(i))
+firstpart = []
+secondpart = []
+x = 0
+for n, item in enumerate(input_list):
+    if n == 0 + x:
+        firstpart.append(int(item))
+        x = x + 2
+    else:
+        secondpart.append(int(item))
+
+for y in firstpart, secondpart:
+    print(range(y, y))
+    
+
+
+
+
 
